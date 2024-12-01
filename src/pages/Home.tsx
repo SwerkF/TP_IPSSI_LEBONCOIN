84610682ce700413f6cfa564e03f05fcbd44a6c6
swerk <swerk.pro@gmail.com>
2024-12-01 20:13:11 +0100

    Added absolute imports

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,9 @@
-
 const Home = () => {
-
   return (
     <div className="min-h-screen bg-gray-100 w-full flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold">Hello, world!</h1>
     </div>
   );
-}
+};
 
 export default Home;
